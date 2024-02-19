2b713dfee4625d21b5bf5f8da8b376b1d02bd091
Chepet <chepet.pg@gmail.com>
2024-02-20 07:45:12 +0900

    Add ClassDiagramSupport.cs ファイルの追加
    Mainメソッド

---
+++ ClassDiagramSupport/ClassDiagramSupport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ClassDiagramSupport
+{
+	public static void Main(string[] args)
+	{
+		//ClassLineTextToCsv classLineTextToCsv = new ClassLineTextToCsv();
+		//classLineTextToCsv.ExcuteSyntaxNodeVer();
+
+		MethodLineTextToCsv methodLineTextToCsv = new MethodLineTextToCsv();
+		methodLineTextToCsv.ExecuteSummaryToCsv();
+
+	}
+}
